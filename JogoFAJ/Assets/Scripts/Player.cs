15ac9953ef2a406e1e871973bdc62f80021d03f7
Rafael212001 <rafael_ferreira2013@hotmail.com.br>
2022-04-08 21:00:09 -0300

    Melhorando a Movimentacao com o Cesar

--- JogoFAJ/Assets/Scripts/Player.cs
+++ JogoFAJ/Assets/Scripts/Player.cs
@@ -4,78 +4,101 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float Speed;
-    public float JumpForce;
-    private Rigidbody2D rigi;
-    private Animator anim;
-    public bool isJumping;
-    public bool doubleJump;
+    public float speed = 10.0f;
+    public float jumpForce = 10.0f;
+    public float fallGravityScale = 3.0f;
+    public LayerMask groundLayer;
+    public float radius = 10.0f;
+    public Vector3 offset;
+
+    private Rigidbody2D _rigidbody;
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+
+    private static readonly int SpeedHash = Animator.StringToHash("Speed");
+    private static readonly int GroundedHash = Animator.StringToHash("Grounded");
+
+    private Vector2 _moviment = Vector2.zero;
+    private bool _isJumping = false;
+    private bool _isGrounded = false;
+
+    // Awake is called when the script instance is being loaded
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _animator = GetComponent<Animator>();
+        //playerState.Initialize();
+    }
+
 
     // Start is called before the first frame update
     void Start()
     {
-        rigi = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        //GameManager.Instace.LoadScene("");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
-        Jump();
+        // Capturamos o valor de input do jogador no eixo horizontal.
+        float inputX = Input.GetAxisRaw("Horizontal");
+        // Criamos o vetor de movimento de acordo com o input.
+        _moviment = new Vector2(inputX, 0.0f) * speed;
+        // flipamos a imagem de acordo com a direcao do controle.
+        if (_moviment.sqrMagnitude > 0.1f)
+            _spriteRenderer.flipX = !(inputX > 0.1f);
+
+        // Atualiza variavel de estado de salto.
+        if (!_isJumping)
+            _isJumping = Input.GetButtonDown("Jump");
+
+        // Atualiza parametro na maquina de estados de animacao.
+        _animator.SetFloat(SpeedHash, Mathf.Abs(_rigidbody.velocity.x));
+        _animator.SetBool(GroundedHash, _isGrounded);
     }
 
-    void Move() 
+    private void FixedUpdate()
     {
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * Speed;
+        _isGrounded = Physics2D.OverlapCircle(this.transform.position + offset,
+                                                            radius, groundLayer);
 
-        if(Input.GetAxis("Horizontal") > 0f)
+        if (_moviment.sqrMagnitude > 0.0f &&
+            Mathf.Abs(_rigidbody.velocity.y) < 0.01f)
         {
-            anim.SetBool("walk", true);
-            transform.eulerAngles = new Vector3(0f, 0f, 0f);
-        }
-        if (Input.GetAxis("Horizontal") < 0f)
-        {
-            anim.SetBool("walk", true);
-            transform.eulerAngles = new Vector3(0f, 180f, 0f);
-        }
-        if (Input.GetAxis("Horizontal") == 0f)
-        {
-            anim.SetBool("walk", false);
+            _rigidbody.AddForce(_moviment, ForceMode2D.Force);
         }
 
-    }
-
-    void Jump()
-    {
-        if(Input.GetButtonDown("Jump")  )
+        if (_isJumping && _isGrounded)
         {
-            if (!isJumping)
-            {
-                rigi.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
-                doubleJump = true;
-                anim.SetBool("jump", true);
-            }
-            
-           
+            _isJumping = false;
+            _rigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
-    }
 
-     void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == 8)
+        if (_rigidbody.velocity.y < -0.01f)
         {
-            isJumping = false;
-            anim.SetBool("jump", true);
+            _rigidbody.gravityScale = fallGravityScale;
+        }
+        else
+        {
+            if (_rigidbody.gravityScale > 2.0f)
+                _rigidbody.gravityScale = 2.0f;
         }
     }
 
-    void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.layer == 8)
+        //if (collision.CompareTag("Damage"))
         {
-            isJumping = true;
+            //playerState.TakeHit();
         }
+    }
+
+
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position + offset, radius);
     }
 }
