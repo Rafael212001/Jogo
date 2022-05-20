ec9d8938a30034fe6cc774f86eda6f4efb7a811d
Rafael212001 <rafael_ferreira2013@hotmail.com.br>
2022-05-20 18:02:47 -0300

    Alguns comites q nao dei

---
+++ JogoFAJ/Assets/Scripts/inimigo.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inimigo : MonoBehaviour
+{
+    private Rigidbody2D rig;
+    private Animator anim;
+
+    public float speed;
+
+    public Transform rightcol;
+    public Transform leftcol;
+    public Transform headcol;
+
+    public bool colliding;
+
+    public LayerMask layer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        rig.velocity = new Vector2(speed, rig.velocity.y);
+
+        colliding = Physics2D.Linecast(rightcol.position, leftcol.position, layer);
+
+        if (colliding)
+        {
+            transform.localScale = new Vector2(transform.localScale.x * -1f, transform.localScale.y);
+            speed *= -1f;
+        }
+    }
+}
